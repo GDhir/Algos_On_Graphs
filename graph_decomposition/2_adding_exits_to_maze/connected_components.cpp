6a576f8ed69ef9377394c1f8f346150e7f4e5608
Gaurav Dhir <mail2gauravdhir@gmail.com>
2021-09-05 00:42:42 -0400

    Better DFS for finding number of graph components

--- graph_decomposition/2_adding_exits_to_maze/connected_components.cpp
+++ graph_decomposition/2_adding_exits_to_maze/connected_components.cpp
@@ -3,6 +3,15 @@
 #include <stack>
 #include <unordered_set>
 
+/* Algorithm to find number of connected components given a graph G: The algorithm
+   marks all nodes as unexplored initially. As soon as a node is visited, it is 
+   removed from the unexplored table. 
+
+   The algorithm iteratively checks after the program stack is empty whether there 
+   are any unexplored nodes remaining. If there are any nodes remaining, it adds them to 
+   the program stack to explore the remaining graph component. It also updates the number of 
+   components accordingly. */
+
 using namespace std;
 using std::vector;
 using std::pair;
@@ -11,8 +20,6 @@ int number_of_components(vector< vector<int> > &adj) {
   int res = 0;
   //write your code here
   stack<int> stk;
-  unordered_set<int> visited;
-
   unordered_set<int> unexplored;
 
   for(int i = 0; i < adj.size(); i++) {
@@ -22,22 +29,29 @@ int number_of_components(vector< vector<int> > &adj) {
   while( !unexplored.empty() ) {
     res += 1;
     stk.push(*unexplored.begin());
-
     while( !stk.empty() ) {
 
       auto val = stk.top();
-      stk.pop();
-      visited.insert(val);
+      int idx = 0;
 
-      for( auto& item: adj[val] ) {
+      while( idx < adj[val].size() ) {
 
-        if( visited.find(item) == visited.end() ) {
+        auto item = unexplored.find( adj[val][idx] );
+        if( item != unexplored.end() ) {
+          stk.push( adj[val][idx] );
+          unexplored.erase( adj[val][idx] );
+          break;
+        }
+        idx += 1;
+      }
 
-          stk.push(item);
-
+      if( idx == adj[val].size() ) {
+        stk.pop();
+        auto item = unexplored.find( val );
+        if( item != unexplored.end() ) {
+          unexplored.erase(val);
         }
       }
-      unexplored.erase(val);
     }
   }
 
